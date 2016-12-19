042f57efa02cf9d551eaf2ceee4ba3560b453f02
Gabriel Dahia <gdahia@gmail.com>
2016-12-19 19:42:29 -0300

    error stopping

--- src/scanner_demo.cpp
+++ src/scanner_demo.cpp
@@ -1,5 +1,6 @@
-#include <lex.hpp>
 #include <iostream>
+
+#include <lex.hpp>
 
 int main(int argc, char ** argv) {
     if (argc <= 1) {
@@ -7,7 +8,9 @@ int main(int argc, char ** argv) {
         return 1;
     }
     lex::Scanner scanner(argv[1]);
-    std::vector<lex::Token> all_tokens = scanner.get_all_tokens();
-    for (const lex::Token & t : all_tokens)
+    for (const lex::Token & t : scanner.get_all_tokens()) {
         std::cout << t << std::endl;
+        if (t.type == lex::Token::Type::ERROR)
+            return 1;
+    }
 }
