c08378408cc9bb58884684b0b05ff5426d2547b7
Gabriel Dahia <gdahia@gmail.com>
2017-03-08 10:22:05 -0300

    check if input file is open
    standartization of errors

--- src/def_compiler.cpp
+++ src/def_compiler.cpp
@@ -11,8 +11,12 @@ extern int yyparse();
 extern FILE * yyin;
 
 int main(const int argc, const char ** argv) {
-    if (argc > 1)
+    if (argc > 1) {
+        std::ifstream in(argv[1]);
+        if (!in.is_open())
+            throw std::runtime_error("Unable to open file " + std::string(argv[1]));
         yyin = fopen(argv[1], "r");
+    }
     if (argc > 2) {
         std::ofstream out(argv[2]);
         if (out.is_open()) {
@@ -20,7 +24,7 @@ int main(const int argc, const char ** argv) {
             out << program << std::endl;
         }
         else
-            throw std::runtime_error("Error: unable to open file " + std::string(argv[2]));
+            throw std::runtime_error("Unable to open file " + std::string(argv[2]));
     }
     else {
         yyparse();
